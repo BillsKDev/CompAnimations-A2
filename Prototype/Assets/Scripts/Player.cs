227121cdd1783c5a020732cdf484d4fb725276b1
Bilal Khan <115832016+BillsKDev@users.noreply.github.com>
2025-11-19 14:50:16 -0500

    Waater and jump anims

--- Prototype/Assets/Scripts/Player.cs
+++ Prototype/Assets/Scripts/Player.cs
@@ -4,10 +4,14 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float _turnSpeed = 10f;
     [SerializeField] float _moveSpeed = 5f;
+    [SerializeField] float _jumpForce = 5f;
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] float _groundCheckDistance = 0.1f;
 
     Rigidbody _rigidbody;
     Animator _animator;
-    
+    public bool _isGrounded;
+
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -16,6 +20,8 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
+        CheckGrounded();
+        
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
@@ -23,15 +29,45 @@ public class Player : MonoBehaviour
             vertical *= 2f;
 
         Vector3 velocity = new Vector3(horizontal, 0, vertical);
-        
+
         if (velocity.magnitude > 0.1f)
         {
             Quaternion targetRotation = Quaternion.LookRotation(velocity);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _turnSpeed * Time.fixedDeltaTime);
+
             Vector3 movementDirection = velocity.normalized * (_moveSpeed * Time.fixedDeltaTime);
             _rigidbody.MovePosition(transform.position + movementDirection);
         }
 
         _animator.SetFloat("Speed", velocity.magnitude, 0.1f, Time.deltaTime);
+    }
+
+    void Update()
+    {
+        if (_isGrounded)
+            _animator.SetBool("Jumping", false);
+        
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _animator.SetBool("Jumping", true);
+        }
+    }
+
+    void CheckGrounded()
+    {
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, _groundCheckDistance + 0.2f, _groundLayer);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water"))
+            _animator.SetBool("Water", true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water"))
+            _animator.SetBool("Water", false);
     }
 }
